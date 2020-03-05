17d12b4428ed9aff32f6d48d11c228c7d8e71287
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-03-04 16:54:32 -0800

    Keeping the deploy.cmd at the root level and moving other scripts to the directories

--- deploy.cmd
+++ deploy.cmd
@@ -4,11 +4,11 @@ echo Current Directory is %CD%
 
 IF "%SITE_ROLE%" == "api" (
   echo API Deployment.
-  call \Kronos-Shifts-Connector\deploy.api.cmd
+  deploy.api.cmd
 ) ELSE (
   IF "%SITE_ROLE%" == "config" (
       echo Config App Deployment.
-    call \Kronos-Shifts-Connector\deploy.config.cmd
+    deploy.config.cmd
   ) ELSE (
     echo You have to set SITE_ROLE setting to either "api" or "config"
     exit /b 1
