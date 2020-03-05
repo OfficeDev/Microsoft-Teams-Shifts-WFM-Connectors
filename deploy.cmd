5702f1a8edc429631e276eb61b1b6cb00a07459e
Pranav Krishnamurthy <v-prkris@microsoft.com>
2020-03-04 17:29:42 -0800

    Having the necessary things properly done - making sure to select the right scripts

--- deploy.cmd
+++ deploy.cmd
@@ -1,16 +1,9 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off 
 
-echo Current Directory is %CD%
-
-IF "%SITE_ROLE%" == "api" (
-  echo API Deployment.
-  deploy.api.cmd
+IF "%DEPLOYMENT_SCRIPT%" neq "" (
+  echo Running the deployment script %DEPLOYMENT_SCRIPT%
+  call %DEPLOYMENT_SCRIPT%
 ) ELSE (
-  IF "%SITE_ROLE%" == "config" (
-      echo Config App Deployment.
-    deploy.config.cmd
-  ) ELSE (
-    echo You have to set SITE_ROLE setting to either "api" or "config"
-    exit /b 1
-  )
+  echo DEPLOYMENT_SCRIPT setting is required
+  exit /b 1
 )
