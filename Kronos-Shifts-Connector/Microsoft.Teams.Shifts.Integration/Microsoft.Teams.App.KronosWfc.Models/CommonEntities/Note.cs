9e3f7d46e7c8dab13bb29ded6fe57f2eb77e1032
SonnyWeightWork <84505082+SonnyWeightWork@users.noreply.github.com>
2021-08-18 14:26:53 +0100

    Create common entities as comments accross all entities share the same models

---
+++ Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.App.KronosWfc.Models/CommonEntities/Note.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Note.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.App.KronosWfc.Models.CommonEntities
+{
+    using System.Xml.Serialization;
+
+    /// <summary>
+    /// This class models the Note.
+    /// </summary>
+    [XmlRoot(ElementName = "Note")]
+    public class Note
+    {
+        /// <summary>
+        /// Gets or sets the Timestamp.
+        /// </summary>
+        [XmlAttribute(AttributeName = "Timestamp")]
+        public string Timestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Text.
+        /// </summary>
+        [XmlAttribute(AttributeName = "Text")]
+        public string Text { get; set; }
+    }
+}
