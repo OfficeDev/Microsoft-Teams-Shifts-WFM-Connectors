1c3ba347a1b08cb6ede54b0dfa65b5562589495e
SonnyWeightWork <84505082+SonnyWeightWork@users.noreply.github.com>
2021-08-27 15:38:01 +0100

    Add an app setting to configure the auth token cache TTL

--- Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.Shifts.Integration.API/Common/AppSettings.cs
+++ Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.Shifts.Integration.API/Common/AppSettings.cs
@@ -192,6 +192,11 @@ namespace Microsoft.Teams.Shifts.Integration.API.Common
         public string KronosQueryDateSpanFormat => this.configuration["KronosQueryDateSpanFormat"];
 
         /// <summary>
+        /// Gets the number of seconds to set the time to live when caching a kronos session token.
+        /// </summary>
+        public string AuthTokenCacheLifetimeInSeconds => this.configuration["AuthTokenCacheLifetimeInSeconds"];
+
+        /// <summary>
         /// Gets the number of org job path section sto use as the activity display name.
         /// </summary>
         public string NumberOfOrgJobPathSectionsForActivityName => this.configuration["NumberOfOrgJobPathSectionsForActivityName"];
