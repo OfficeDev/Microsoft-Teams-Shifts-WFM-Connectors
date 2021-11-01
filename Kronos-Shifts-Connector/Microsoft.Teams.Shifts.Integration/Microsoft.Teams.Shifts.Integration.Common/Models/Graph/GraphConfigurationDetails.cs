04806759ce1cc9760e3a99f748e9f9bc1e60519c
SonnyWeightWork <84505082+SonnyWeightWork@users.noreply.github.com>
2021-11-01 10:44:12 +0000

    Add new graph config model

---
+++ Kronos-Shifts-Connector/Microsoft.Teams.Shifts.Integration/Microsoft.Teams.Shifts.Integration.Common/Models/Graph/GraphConfigurationDetails.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="GraphConfigurationDetails.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Shifts.Integration.BusinessLogic.Models.Graph
+{
+    /// <summary>
+    /// Contains all the necessary configuration details to enable Graph requests.
+    /// </summary>
+    public class GraphConfigurationDetails
+    {
+        /// <summary>
+        /// Gets or sets the TenantId.
+        /// </summary>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ClientId.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ClientSecret.
+        /// </summary>
+        public string ClientSecret { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Instance.
+        /// </summary>
+        public string Instance { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AAD Object Id of the Shifts Admin.
+        /// </summary>
+        public string ShiftsAdminAadObjectId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the MS Graph Access token.
+        /// </summary>
+        public string ShiftsAccessToken { get; set; }
+    }
+}
