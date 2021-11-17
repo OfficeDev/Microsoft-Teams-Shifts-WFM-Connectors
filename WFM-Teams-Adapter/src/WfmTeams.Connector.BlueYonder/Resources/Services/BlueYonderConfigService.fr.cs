14c4e1c133c8ef30c3a8b3bcaab4a33b2862f2ae
SonnyWeightWork <84505082+SonnyWeightWork@users.noreply.github.com>
2021-11-17 11:39:13 +0000

    Remove BY connector

--- WFM-Teams-Adapter/src/WfmTeams.Connector.BlueYonder/Resources/Services/BlueYonderConfigService.fr.cs
+++
@@ -1,7 +0,0 @@
-﻿// ---------------------------------------------------------------------------
-// <copyright file="BlueYonderConfigService.fr.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// ---------------------------------------------------------------------------
-
-// Ensure resx file embedded in root assembly not satellite assembly
